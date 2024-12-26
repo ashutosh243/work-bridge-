78b032cf4e356e175b7c2e8b94ae956b24c505d2
ashutoshgupt243 <vnsashutoshit.gmail.com>
2024-12-26 23:04:38 +0530

    cookies change

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -96,7 +96,7 @@ export const login = async (req, res) => {
             role: user.role,
             profile: user.profile
         }
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'none', Domain:'https://work-bridge.onrender.com'}).json({
+        return res.status(200).cookie("token", token).json({
             message: `Welcome back ${user.fullname}`,
             user,
             success: true,
@@ -107,7 +107,7 @@ export const login = async (req, res) => {
 }
 export const logout = async (req, res) => {
     
-    try{
+    try{    
         return res.status(200).cookie("token", "", { maxAge: 0 }).json({
             message: "Logged out successfully.",
             success: true
