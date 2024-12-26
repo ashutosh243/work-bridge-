820171c912de6c45f5cfc7c80544b9e5bb093d0a
ashutoshgupt243 <vnsashutoshit.gmail.com>
2024-12-26 18:28:08 +0530

    cors update

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:['http://localhost:5173 ','https://work-bridge-1.onrender.com/'],
+    origin:['http://localhost:5173','https://work-bridge-1.onrender.com'],
     credentials:true
 }
 
@@ -25,7 +25,6 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 8000;  
 
 
-// api's
 app.get('/',(req,res)=>{
     return res.json({
         massage:"connected "
